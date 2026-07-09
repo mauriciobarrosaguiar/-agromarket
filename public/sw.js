e97b37b9af2d91117a8a3b7c9f8eeadd03e1805c
mauriciobarrosaguiar <emsgenericosto@gmail.com>
2026-07-09 10:40:15 -0300

    Atualiza cache apos ajuste responsivo

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'agromarket-v13';
+const CACHE_NAME = 'agromarket-v14';
 const STATIC_ASSETS = ['/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', (event) => {
