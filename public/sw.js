a8e57a795bdeacb214c4d730d8a1d1911e28cf94
mauriciobarrosaguiar <emsgenericosto@gmail.com>
2026-07-09 10:51:55 -0300

    Atualiza cache apos correcao forte da home

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'agromarket-v14';
+const CACHE_NAME = 'agromarket-v15';
 const STATIC_ASSETS = ['/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', (event) => {
