0d386522721097d71e10dab37025bca1acd14b16
mauriciobarrosaguiar <emsgenericosto@gmail.com>
2026-07-09 13:53:27 -0300

    Atualiza cache do Pix automatico

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'agromarket-v17';
+const CACHE_NAME = 'agromarket-v18';
 const STATIC_ASSETS = ['/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', (event) => {
