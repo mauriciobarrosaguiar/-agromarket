a8116094cb70c83270a5639b78d40fb310dfcbc2
mauriciobarrosaguiar <emsgenericosto@gmail.com>
2026-07-08 22:44:40 -0300

    Atualiza cache para texto de documento

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'agromarket-v7';
+const CACHE_NAME = 'agromarket-v8';
 const STATIC_ASSETS = ['/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', (event) => {
