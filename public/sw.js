0663295ce375831f2445a3ae5adde93054b57432
mauriciobarrosaguiar <emsgenericosto@gmail.com>
2026-07-09 18:11:43 -0300

    Atualiza cache das paginas legais e suporte

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'agromarket-v18';
+const CACHE_NAME = 'agromarket-v19';
 const STATIC_ASSETS = ['/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', (event) => {
