8f6b75201a7944f418334f051b93024c013ca84f
mauriciobarrosaguiar <emsgenericosto@gmail.com>
2026-07-08 22:34:20 -0300

    Atualiza cache para painel documentos

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'agromarket-v6';
+const CACHE_NAME = 'agromarket-v7';
 const STATIC_ASSETS = ['/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', (event) => {
