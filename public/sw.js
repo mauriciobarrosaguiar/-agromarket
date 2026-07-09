ac8ca90ded7c67f89e78858450a7a2a236f4006c
mauriciobarrosaguiar <emsgenericosto@gmail.com>
2026-07-09 20:54:03 -0300

    Atualiza cache do feedback visual

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'agromarket-v20';
+const CACHE_NAME = 'agromarket-v21';
 const STATIC_ASSETS = ['/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', (event) => {
