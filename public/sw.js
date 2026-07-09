47b0e395591f57928eb0f3e89c084905765650d4
mauriciobarrosaguiar <emsgenericosto@gmail.com>
2026-07-08 22:54:55 -0300

    Atualiza cache para aprovacao de selfie

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'agromarket-v8';
+const CACHE_NAME = 'agromarket-v9';
 const STATIC_ASSETS = ['/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', (event) => {
