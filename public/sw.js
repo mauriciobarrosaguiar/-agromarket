c0b9f60b9d65b96362614b07d668f9dfe283b660
mauriciobarrosaguiar <emsgenericosto@gmail.com>
2026-07-10 21:40:51 -0300

    Atualiza cache apos ajustes de perfil e app

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'agromarket-v27';
+const CACHE_NAME = 'agromarket-v28';
 const STATIC_ASSETS = ['/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', (event) => {
