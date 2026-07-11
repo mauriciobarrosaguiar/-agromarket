45a726b64d7807c3e63f4bfb996629322917af8f
mauriciobarrosaguiar <emsgenericosto@gmail.com>
2026-07-10 21:45:49 -0300

    Atualiza cache apos corrigir modal WhatsApp

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'agromarket-v28';
+const CACHE_NAME = 'agromarket-v29';
 const STATIC_ASSETS = ['/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', (event) => {
