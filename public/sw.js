25240c027af23cebafe02b4e94d625f8140dca1b
mauriciobarrosaguiar <emsgenericosto@gmail.com>
2026-07-12 14:26:24 -0300

    Atualiza cache apos corrigir cadastro

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'agromarket-v29';
+const CACHE_NAME = 'agromarket-v30';
 const STATIC_ASSETS = ['/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', (event) => {
