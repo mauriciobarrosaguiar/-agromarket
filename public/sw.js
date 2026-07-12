d30a3077f639059611dfe7c6e530b5a4b4038fa2
mauriciobarrosaguiar <emsgenericosto@gmail.com>
2026-07-12 17:50:16 -0300

    Atualiza cache apos busca de cliente na vitrine

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'agromarket-v32';
+const CACHE_NAME = 'agromarket-v33';
 const STATIC_ASSETS = ['/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', (event) => {
