2730f38f4cb3cabef5402266c1a5b4c5111a7d4b
mauriciobarrosaguiar <emsgenericosto@gmail.com>
2026-07-15 16:43:18 -0300

    Atualiza cache apos remover textos do piloto

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'agromarket-v36';
+const CACHE_NAME = 'agromarket-v37';
 const STATIC_ASSETS = ['/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', (event) => {
